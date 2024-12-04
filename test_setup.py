9af83a0d371455e4a8d62ad84d5622cb28f61ae4
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2024-12-04 23:06:18 +0900

    modified

--- test_setup.py
+++
@@ -1,5 +0,0 @@
-from DiversifyApproach_package.calculate_EXVAL import DiversifyApproach
-
-# 簡単なインスタンス作成テスト
-instance = DiversifyApproach(ticker="AAPL", specialized_tickers=[], data={})
-print(instance)
