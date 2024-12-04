9738152185c54be2476d231fda54bce4ee6f48ed
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2024-12-04 17:18:31 +0900

    Fixed setup.py for package installation

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DiversifyApproach_package",  # パッケージの名前
+    name="DiversifyApproach_package", 
     version="0.0.1",
     author="Takumi Sakamoto",
     author_email="takumi.saka.mo0107@gmail.com",
-    license='MIT',
-    description="A package to calculate beta values using finance data.",
+    license="MIT",
+    description="A package to calculate EXVAL values using finance data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/takumi-saka-mo/DEVIRSIFY_Analysis",
@@ -18,10 +18,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=['my_finance'],  # my_finance パッケージを指定
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},  # 僕はここ
     python_requires=">=3.7",
     install_requires=[
         "pandas",
-        "yfinance"
+        "yfinance",
     ],
 )
