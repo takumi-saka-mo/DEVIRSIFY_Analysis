3a681dbcc1b6b367f79a12c8e7e4e2d55c2f6b8e
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2024-12-04 18:52:17 +0900

    modified

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setuptools.setup(
     version="1.0.0",
     author="Takumi Sakamoto",
     author_email="takumi.saka.mo0107@gmail.com",
-    license="MIT",
     description="A package to calculate EXVAL values using finance data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -18,7 +17,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(where="src"), 
+    packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},  # 僕はここ
     python_requires=">=3.7",
     install_requires=["pandas", "yfinance"],
