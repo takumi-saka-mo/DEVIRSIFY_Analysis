7d16a6a800895ce9d6ba5f275ecf8358d9460e14
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2024-12-05 14:00:12 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -4,22 +4,25 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DiversifyApproach_package",
-    version="1.0.0",
+    name="DiversifyApproach_package",  # パッケージの名前
+    version="0.0.1",
     author="Takumi Sakamoto",
     author_email="takumi.saka.mo0107@gmail.com",
+    license='MIT',
     description="A package to calculate EXVAL values using finance data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/takumi-saka-mo/DEVIRSIFY_Analysis",
+    url="https://github.com/takumi-saka-mo/my_module",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=['src'],
     python_requires=">=3.7",
-    install_requires=["pandas", "yfinance"],
-    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "numpy",
+        "yfinance"
+    ],
 )
